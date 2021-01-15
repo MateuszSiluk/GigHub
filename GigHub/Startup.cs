11b2f8ec4018d6d6124699a544d8a9a454aa18ba
Mateusz Siluk <mati169@gmail.com>
2021-01-15 22:05:48 +0000

    Adding-a-Drop-down-List-to-Navbar

--- GigHub/Startup.cs
+++ GigHub/Startup.cs
@@ -34,6 +34,9 @@ namespace GigHub
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddRazorPages()
+        .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
